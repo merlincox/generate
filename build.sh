883d13ead74dc01427948791750230741b378ff8
Merlin Cox <merlin.cox@denma.digital>
2018-03-15 13:17:54 +0000

    Improve simple build Tweak struct titling

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 output_dir=/usr/local/bin
 output_exe=schema-generator
+
+# Simple example build script
 
 go build -o $output_dir/$output_exe cmd/schema-generate/main.go
